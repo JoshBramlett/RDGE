fc40344bd8180954be8e9a1c2878397183a6070f
JoshBramlett <bramsquad@mac.com>
2018-02-20 00:05:03 -0600

    Updated TODO; minor cleanup

--- include/rdge/assets.hpp
+++ include/rdge/assets.hpp
@@ -8,6 +8,7 @@
 #include <rdge/assets/tilemap/property.hpp>
 #include <rdge/assets/tilemap/tilemap.hpp>
 #include <rdge/assets/pack_file.hpp>
+#include <rdge/assets/shared_asset.hpp>
 #include <rdge/assets/spritesheet.hpp>
 #include <rdge/assets/surface.hpp>
 #include <rdge/assets/tileset.hpp>
--- src/assets/pack_file.cpp
+++ src/assets/pack_file.cpp
@@ -1,8 +1,8 @@
 #include <rdge/assets/pack_file.hpp>
-#include <rdge/assets/file_formats/asset_pack.hpp>
 #include <rdge/assets/spritesheet.hpp>
 #include <rdge/assets/surface.hpp>
 #include <rdge/assets/tileset.hpp>
+#include <rdge/assets/tilemap/tilemap.hpp>
 #include <rdge/util/memory/alloc.hpp>
 #include <rdge/util/compiler.hpp>
 #include <rdge/util/json.hpp>
