d85d031d692a9b58956b473d65183da4a3f8c015
JoshBramlett <bramsquad@mac.com>
2018-06-26 23:05:27 -0500

    Indoor asset upload

---
+++ sandbox/chrono/res/tilesets/indoors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="indoors" tilewidth="16" tileheight="16" margin="1" tilecount="961" columns="30">
+ <image source="indoors.png" width="482" height="482"/>
+</tileset>
